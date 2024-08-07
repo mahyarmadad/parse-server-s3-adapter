146651330850483a2a6ce6048ec17d17ca330cf1
mahyarmadad <erfansam8891@yahoo.com>
2024-08-07 10:10:57 +0330

    update createFile to aws v3

--- index.js
+++ index.js
@@ -2,14 +2,17 @@
 //
 // Stores Parse files in AWS S3.
 
-const { S3Client, CreateBucketCommand } = require('@aws-sdk/client-s3');
-const optionsFromArguments = require('./lib/optionsFromArguments');
+const { S3Client, CreateBucketCommand } = require("@aws-sdk/client-s3");
+const optionsFromArguments = require("./lib/optionsFromArguments");
 
-const awsCredentialsDeprecationNotice = function awsCredentialsDeprecationNotice() {
-  // eslint-disable-next-line no-console
-  console.warn('Passing AWS credentials to this adapter is now DEPRECATED and will be removed in a future version',
-    'See: https://github.com/parse-server-modules/parse-server-s3-adapter#aws-credentials for details');
-};
+const awsCredentialsDeprecationNotice =
+  function awsCredentialsDeprecationNotice() {
+    // eslint-disable-next-line no-console
+    console.warn(
+      "Passing AWS credentials to this adapter is now DEPRECATED and will be removed in a future version",
+      "See: https://github.com/parse-server-modules/parse-server-s3-adapter#aws-credentials for details"
+    );
+  };
 
 const serialize = (obj) => {
   const str = [];
@@ -18,19 +21,25 @@ const serialize = (obj) => {
       str.push(`${encodeURIComponent(key)}=${encodeURIComponent(obj[key])}`);
     }
   });
-  return str.join('&');
+  return str.join("&");
 };
 
-function buildDirectAccessUrl(baseUrl, baseUrlFileKey, presignedUrl, config, filename) {
+function buildDirectAccessUrl(
+  baseUrl,
+  baseUrlFileKey,
+  presignedUrl,
+  config,
+  filename
+) {
   let directAccessUrl;
-  if (typeof baseUrl === 'function') {
+  if (typeof baseUrl === "function") {
     directAccessUrl = `${baseUrl(config, filename)}/${baseUrlFileKey}`;
   } else {
     directAccessUrl = `${baseUrl}/${baseUrlFileKey}`;
   }
 
   if (presignedUrl) {
-    directAccessUrl += presignedUrl.substring(presignedUrl.indexOf('?'));
+    directAccessUrl += presignedUrl.substring(presignedUrl.indexOf("?"));
   }
 
   return directAccessUrl;
@@ -76,9 +85,7 @@ class S3Adapter {
         accessKeyId: options.accessKey,
         secretAccessKey: options.secretKey,
       };
-    } else if (options.credentials)
-      s3Options.credentials = options.credentials;
-
+    } else if (options.credentials) s3Options.credentials = options.credentials;
 
     if (options.accessKey && options.secretKey) {
       awsCredentialsDeprecationNotice();
@@ -96,11 +103,12 @@ class S3Adapter {
     if (this._hasBucket) return;
 
     try {
-      await this._s3Client.send(new CreateBucketCommand({ Bucket: this._bucket }));
+      await this._s3Client.send(
+        new CreateBucketCommand({ Bucket: this._bucket })
+      );
       this._hasBucket = true;
     } catch (error) {
-      if (error.name === "BucketAlreadyOwnedByYou")
-        this._hasBucket = true;
+      if (error.name === "BucketAlreadyOwnedByYou") this._hasBucket = true;
       else throw error;
     }
   }
@@ -117,13 +125,13 @@ class S3Adapter {
       params.Key = this._bucketPrefix + this._generateKey(filename);
     }
     if (this._fileAcl) {
-      if (this._fileAcl === 'none') {
+      if (this._fileAcl === "none") {
         delete params.ACL;
       } else {
         params.ACL = this._fileAcl;
       }
     } else if (this._directAccess) {
-      params.ACL = 'public-read';
+      params.ACL = "public-read";
     }
     if (contentType) {
       params.ContentType = contentType;
@@ -131,70 +139,78 @@ class S3Adapter {
     if (this._globalCacheControl) {
       params.CacheControl = this._globalCacheControl;
     }
-    if (this._encryption === 'AES256' || this._encryption === 'aws:kms') {
+    if (this._encryption === "AES256" || this._encryption === "aws:kms") {
       params.ServerSideEncryption = this._encryption;
     }
-    if (options.metadata && typeof options.metadata === 'object') {
+    if (options.metadata && typeof options.metadata === "object") {
       params.Metadata = options.metadata;
     }
-    if (options.tags && typeof options.tags === 'object') {
+    if (options.tags && typeof options.tags === "object") {
       const serializedTags = serialize(options.tags);
       params.Tagging = serializedTags;
     }
-    return this.createBucket().then(() => new Promise((resolve, reject) => {
-      this._s3Client.upload(params, (err, response) => {
-        if (err !== null) {
-          return reject(err);
-        }
-        return resolve(response);
-      });
-    }));
+    return this.createBucket().then(
+      () =>
+        new Promise((resolve, reject) => {
+          const command = new PutObjectCommand({
+            Bucket: this._bucket,
+            ...params,
+          });
+          this._s3Client.send(command).then(resolve).catch(reject);
+        })
+    );
   }
 
   deleteFile(filename) {
-    return this.createBucket().then(() => new Promise((resolve, reject) => {
-      const params = {
-        Key: this._bucketPrefix + filename,
-      };
-      this._s3Client.deleteObject(params, (err, data) => {
-        if (err !== null) {
-          return reject(err);
-        }
-        return resolve(data);
-      });
-    }));
+    return this.createBucket().then(
+      () =>
+        new Promise((resolve, reject) => {
+          const params = {
+            Key: this._bucketPrefix + filename,
+          };
+          this._s3Client.deleteObject(params, (err, data) => {
+            if (err !== null) {
+              return reject(err);
+            }
+            return resolve(data);
+          });
+        })
+    );
   }
 
   // Search for and return a file if found by filename
   // Returns a promise that succeeds with the buffer result from S3
   getFileData(filename) {
     const params = { Key: this._bucketPrefix + filename };
-    return this.createBucket().then(() => new Promise((resolve, reject) => {
-      this._s3Client.getObject(params, (err, data) => {
-        if (err !== null) {
-          return reject(err);
-        }
-        // Something happened here...
-        if (data && !data.Body) {
-          return reject(data);
-        }
-        return resolve(data.Body);
-      });
-    }));
+    return this.createBucket().then(
+      () =>
+        new Promise((resolve, reject) => {
+          this._s3Client.getObject(params, (err, data) => {
+            if (err !== null) {
+              return reject(err);
+            }
+            // Something happened here...
+            if (data && !data.Body) {
+              return reject(data);
+            }
+            return resolve(data.Body);
+          });
+        })
+    );
   }
 
   // Generates and returns the location of a file stored in S3 for the given request and filename
   // The location is the direct S3 link if the option is set,
   // otherwise we serve the file through parse-server
   getFileLocation(config, filename) {
-    const fileName = filename.split('/').map(encodeURIComponent).join('/');
+    const fileName = filename.split("/").map(encodeURIComponent).join("/");
     if (!this._directAccess) {
       return `${config.mount}/files/${config.applicationId}/${fileName}`;
     }
 
     const fileKey = `${this._bucketPrefix}${fileName}`;
 
-    let presignedUrl = '';
+    let presignedUrl = "";
     if (this._presignedUrl) {
       const params = { Bucket: this._bucket, Key: fileKey };
       if (this._presignedUrlExpires) {
@@ -202,7 +218,7 @@ class S3Adapter {
       }
       // Always use the "getObject" operation, and we recommend that you protect the URL
       // appropriately: https://docs.aws.amazon.com/AmazonS3/latest/dev/ShareObjectPreSignedURL.html
-      presignedUrl = this._s3Client.getSignedUrl('getObject', params);
+      presignedUrl = this._s3Client.getSignedUrl("getObject", params);
       if (!this._baseUrl) {
         return presignedUrl;
       }
@@ -213,33 +229,42 @@ class S3Adapter {
     }
 
     const baseUrlFileKey = this._baseUrlDirect ? fileName : fileKey;
-    return buildDirectAccessUrl(this._baseUrl, baseUrlFileKey, presignedUrl, config, filename);
+    return buildDirectAccessUrl(
+      this._baseUrl,
+      baseUrlFileKey,
+      presignedUrl,
+      config,
+      filename
+    );
   }
 
   handleFileStream(filename, req, res) {
     const params = {
       Key: this._bucketPrefix + filename,
-      Range: req.get('Range'),
+      Range: req.get("Range"),
     };
-    return this.createBucket().then(() => new Promise((resolve, reject) => {
-      this._s3Client.getObject(params, (error, data) => {
-        if (error !== null) {
-          return reject(error);
-        }
-        if (data && !data.Body) {
-          return reject(data);
-        }
-        res.writeHead(206, {
-          'Accept-Ranges': data.AcceptRanges,
-          'Content-Length': data.ContentLength,
-          'Content-Range': data.ContentRange,
-          'Content-Type': data.ContentType,
-        });
-        res.write(data.Body);
-        res.end();
-        return resolve(data.Body);
-      });
-    }));
+    return this.createBucket().then(
+      () =>
+        new Promise((resolve, reject) => {
+          this._s3Client.getObject(params, (error, data) => {
+            if (error !== null) {
+              return reject(error);
+            }
+            if (data && !data.Body) {
+              return reject(data);
+            }
+            res.writeHead(206, {
+              "Accept-Ranges": data.AcceptRanges,
+              "Content-Length": data.ContentLength,
+              "Content-Range": data.ContentRange,
+              "Content-Type": data.ContentType,
+            });
+            res.write(data.Body);
+            res.end();
+            return resolve(data.Body);
+          });
+        })
+    );
   }
 }
 
